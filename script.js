0b3ef4dda66dedb0d8d6da5971d260ab36983bc8
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 22:12:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let calculate=document.getElementById('submit');
+	calculate.addEventListener('click',volume_sphere)
 function volume_sphere() {
     //Write your code here
-  
+  let radius=document.getElementById("radius");
+	let result=4/3*3.14*(radius.value)*(radius.value);
+	result = result.toFixed(4);
+	let volume=document.getElementById("volume");
+	volume.value=result;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
